98509134ad05e5586dece5f8b337c80393a6b222
shubhamkr7658 <shubhamkr76588@gmail.com>
2026-05-03 10:43:34 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -15,15 +15,25 @@ def home():
 
 @app.route("/predict", methods=["POST"])
 def predict():
-    data = request.json
-    df = pd.DataFrame([data])
+    try:
+        data = request.json
+        print("Received:", data)
 
-    prediction = model.predict(df)[0]
-    prediction = max(0, min(100, prediction))
+        df = pd.DataFrame([data])
 
-    return jsonify({   # ✅ NOW INSIDE FUNCTION
-        "risk": round(prediction, 2)
-    })
+        # FIX column spacing issue
+        df.columns = df.columns.str.strip()
+
+        prediction = model.predict(df)[0]
+        prediction = max(0, min(100, prediction))
+
+        return jsonify({
+            "risk": round(prediction, 2)
+        })
+
+    except Exception as e:
+        print("ERROR:", str(e))
+        return jsonify({"error": str(e)}), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
