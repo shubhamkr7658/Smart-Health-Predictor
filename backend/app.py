75fb0760ea70e370b217cdcefc1d5438e7bd7957
shubhamkr7658 <shubhamkr76588@gmail.com>
2026-05-03 10:55:44 +0530

    app.py

--- backend/app.py
+++ backend/app.py
@@ -17,23 +17,22 @@ def home():
 def predict():
     try:
         data = request.json
-        print("Received:", data)
 
         df = pd.DataFrame([data])
 
-        # FIX column spacing issue
+        # fix column names (important)
         df.columns = df.columns.str.strip()
 
         prediction = model.predict(df)[0]
-        prediction = max(0, min(100, prediction))
 
         return jsonify({
-            "risk": round(prediction, 2)
+            "risk": float(prediction)
         })
 
     except Exception as e:
-        print("ERROR:", str(e))
-        return jsonify({"error": str(e)}), 500
+        return jsonify({
+            "error": str(e)
+        }), 500
 
 if __name__ == "__main__":
     app.run(debug=True)
